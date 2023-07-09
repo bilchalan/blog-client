76982ad5de84ae7d8b36f9572ba1fe0b490f5413
Monir U Zaman <87598990+bilchalan@users.noreply.github.com>
2023-07-10 01:22:39 +0600

    Update rootApi.ts

--- src/features/rootApi.ts
+++ src/features/rootApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const rootApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://bil-blog-server.onrender.com/api/v1',
+    baseUrl: 'http://localhost:3500/api/v1',
     credentials: 'include',
   }),
   tagTypes: [
